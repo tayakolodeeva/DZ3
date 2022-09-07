5172b304abe1e75d4820a1b29203dd49eabddf64
Taya <tayakolodeeva@yandex.ru>
2022-09-07 20:01:08 +0300

    fix4

---
+++ program2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double GetDistanceTwoPoint(int x1, int y1, int z1, int x2, int y2, int z2) 
+{
+    double distan = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+    return distan;
+}
+
+Console.Write("Введите X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = GetDistanceTwoPoint(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Расстояние между точками ({x1}, {y1}, {z1}) и ({x2}, {y2}, {z2}) равно {Math.Round (result, 2,MidpointRounding.ToNegativeInfinity)}");
---
+++ program3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int[] GetSquaresNumer(int numer)
+{
+    int[] cubeNumers = new int[numer];
+
+    for (int i = 1; i <= numer; i++)
+    {
+        cubeNumers[i - 1] = i * i * i;
+    }
+
+    return cubeNumers;
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] result = GetSquaresNumer(num);
+for (int i = 0; i < result.Length; i++) {
+    Console.WriteLine(result[i]);
+}
